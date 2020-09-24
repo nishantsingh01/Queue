5d2236a7e1cae2654ea8e4c2fe9cd1a265226528
nishantsingh01 <69955342+nishantsingh01@users.noreply.github.com>
2020-09-24 13:57:36 +0530

    Add files via upload

---
+++ LinearQueueUsingarray.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+int queue[200], n = 200, front = - 1, rear = - 1;
+void Insert() {
+   int val;
+   if (rear == n - 1)
+   cout<<"Queue Overflow"<<endl;
+   else {
+      if (front == - 1)
+      front = 0;
+      cout<<"Insert the element in queue : "<<endl;
+      cin>>val;
+      rear++;
+      queue[rear] = val;
+   }
+}
+void Delete() {
+   if (front == - 1 || front > rear) {
+      cout<<"Queue Underflow ";
+      return ;
+   } else {
+      cout<<"Element is Deleted from Queue is : "<< queue[front] <<endl;
+      front++;;
+   }
+}
+void Display() {
+   if (front == - 1)
+   cout<<"Queue is empty"<<endl;
+   else {
+      cout<<"Queue elements are : ";
+      for (int i = front; i <= rear; i++)
+      cout<<queue[i]<<" ";
+         cout<<endl;
+   }
+}
+int main() {
+   int ch;
+   cout<<"\n Press 1 for Insertion in Queue";
+   cout<<"\n Press 2 for Deletion in Queue";
+   cout<<"\n Press 3 for Display all the elements of the Queue";
+   cout<<"\n Press 4 for Exit";
+   do {
+      cout<<"\n Please Enter your choice : "<<endl;
+      cin>>ch;
+      switch (ch) {
+         case 1: Insert();
+         break;
+         case 2: Delete();
+         break;
+         case 3: Display();
+         break;
+         case 4: cout<<"Exit"<<endl;
+         break;
+         default: cout<<"Invalid choice"<<endl;
+      }
+   } while(ch!=4);
+   return 0;
+}
